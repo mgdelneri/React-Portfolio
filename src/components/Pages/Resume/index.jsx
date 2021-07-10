f41cca27dd94580c1011fd722ceb9dc1c20800b0
Marina <mgdelneri@gmail.com>
2021-07-10 13:04:13 -0500

    Added proficiencies to index.jsx in Resume component

--- src/components/Pages/Resume/index.jsx
+++ src/components/Pages/Resume/index.jsx
@@ -17,6 +17,33 @@ const Resume = () => {
       >
         Download Marina Delneri's Résumé
       </a>
+      <section className="tech">
+        <div id="frontEnd">
+          <h3>Front-End Proficiencies</h3>
+          <br></br>
+          <ul>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>JavaScript</li>
+            <li>jQuery</li>
+            <li>Bootstrap</li>
+            <li>React</li>
+          </ul>
+        </div>
+        <div id="backEnd">
+          <h3>Back-End Proficiencies</h3>
+          <br></br>
+          <ul>
+            <li>APIs</li>
+            <li>Node</li>
+            <li>Express</li>
+            <li>MySQL, Sequelize</li>
+            <li>MongoDB, Mongoose</li>
+            <li>REST</li>
+            <li>GraphQL</li>
+          </ul>
+        </div>
+      </section>
       <Footer />
     </div>
   );
