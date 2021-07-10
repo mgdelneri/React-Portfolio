1bcc6203bd7c6c8525bcbf036dbe35e8c2be2d54
Marina <mgdelneri@gmail.com>
2021-07-09 20:29:21 -0500

    Had not saved file for last commit

--- src/components/Pages/Contact/index.jsx
+++ src/components/Pages/Contact/index.jsx
@@ -5,10 +5,54 @@ import Footer from "../../Footer";
 import { validateEmail } from "../../../utils/helpers";
 
 const Contact = () => {
+  const [formState, setFormState] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const { name, email, message } = formState;
+
+  function handleChange(e) {
+    if (e.target.name === "email") {
+      const isValid = validateEmail(e.target.value);
+      if (!isValid) {
+        setErrorMessage("Your email is invalid.");
+      } else {
+        if (!e.target.value.length) {
+          setErrorMessage(`${e.target.name} is required.`);
+        } else {
+          setErrorMessage("");
+        }
+      }
+    }
+
+    if (!errorMessage) {
+      setFormState({ ...formState, [e.target.name]: e.target.value });
+    }
+  }
+
+  function handleBlank(e) {
+    if (e.target.name === "Name" || e.target.name === "Message") {
+      if (!e.target.value.length) {
+        setErrorMessage(`${e.target.name} is required.`);
+      } else {
+        setErrorMessage("");
+      }
+    }
+
+    if (!errorMessage) {
+      setFormState({ ...formState, [e.target.name]: e.target.value });
+    }
+  }
+
   return (
     <div>
       <h1 id="contactMe">Contact Me</h1>
       <Navigation />
+
       <Footer />
     </div>
   );
